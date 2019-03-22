ad16667ae2b08f3ccb6abc0185e25a4cc078c16e
madkii <alexisroux63@gmail.com>
2019-03-22 14:28:12 +0100

    bdd

--- bdd.sql
+++ bdd.sql
@@ -1,28 +1,43 @@
-CREATE DATABASE IF NOT EXISTS bdd;
-USE bdd;
+CREATE DATABASE tp09web;
+USE tp09web;
 
-CREATE TABLE IF NOT EXISTS theme (
-    idTheme int(11) AUTO_INCREMENT NOT NULL,
-    theme varchar(64),
-    PRIMARY KEY(idTheme)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS oeuvre (
-    idOeuvre int(11) AUTO_INCREMENT NOT NULL,
-    oeuvre varchar(64),
-    annee int(11),
-    PRIMARY KEY(idOeuvre)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS utilisateur (
-    idUtilisateur int(11) AUTO_INCREMENT NOT NULL,
-    isAdmin boolean,
+CREATE TABLE IF NOT EXISTS Utilisateur(
+    idUtilisateur INT(11) NOT NULL AUTO_INCREMENT,
+    identifiant VARCHAR(64),
+    motDePasse VARCHAR(64),
+    nom VARCHAR(64),
+    prenom VARCHAR(64),
     PRIMARY KEY(idUtilisateur)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE oeuvre
-ADD CONSTRAINT oeuvre_idTheme
-FOREIGN KEY(idTheme)
-REFERENCES theme(idTheme);
+CREATE TABLE IF NOT EXISTS Oeuvre (
+    idOeuvre INT(11) NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(64),
+    prix FLOAT,
+    lien TEXT,
+    annee INT(11),
+    PRIMARY KEY(idOeuvre)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE 
+CREATE TABLE IF NOT EXISTS Theme (
+    idTheme INT(11) NOT NULL AUTO_INCREMENT,
+    libelle VARCHAR(64),
+    PRIMARY KEY(idTheme)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS ThemeOeuvre (
+    idTheme INT(11),
+    idOeuvre INT(11)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE ThemeOeuvre
+ADD CONSTRAINT ThemeOeuvre_idOeuvre
+FOREIGN KEY(idOeuvre)
+REFERENCES Oeuvre(idOeuvre) ON DELETE CASCADE;
+ALTER TABLE ThemeOeuvre
+ADD CONSTRAINT ThemeOeuvre_idTheme
+FOREIGN KEY(idTheme)
+REFERENCES Theme(idTheme) ON DELETE CASCADE;
+
+INSERT INTO Utilisateur(identifiant, motDePasse, nom, prenom) VALUES
+('Admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'SPLINTER', 'Michelangelo');

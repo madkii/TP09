37f1c19aac48c16dc9dab13fa82f07f4251eb65c
madkii <alexisroux63@gmail.com>
2019-04-12 09:10:22 +0200

    oui

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" /> 
+		<title>Connexion</title>
+    </head>
+    <body>
+		<div class=presentation>Image de presentation</div>
+		<div class=page>
+			<div class=LECENTRE>
+				<div class=description> 
+					<div class=PHOTO> deuxieme ok </div>
+					<div class=TITRE> troisieme ok </div>
+					<div class=TEXTE> quatrieme ok </div>
+					<div class=OEUVRES> Mes oeuvres </div>
+					<div class=FILTRE> FILTRE</div>
+					<div class=CARRE1> </div>
+					<div class=CARRE2> </div>
+					<div class=CARRE3> </div>
+					<div class=CARRE4> </div>
+					<div class=CARRE5> </div>
+					<div class=CARRE6> </div>
+				</div>
+			</div>
+		</div>
+        <div class=conteneur>
+            <div class=contenu>
+                <div class=presentation>
+				
+				
+                    
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
